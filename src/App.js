ff1744bcae1c89624402dd80f46437da65fb914e
Juan Jojoa <juanjojoag@gmail.com>
2023-11-10 16:05:10 +1100

    addtextbox

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [url, setUrl] = useState('');
+
+  const handleAnalysis = () => {
+    // Image analysis logic goes here
+  };
+
+  const handleGeneration = () => {
+    // Image generation logic goes here
+  };
+
+  return (
+    <div>
+      <h1>Image Analyzer and Generator</h1>
+      <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} placeholder="Enter image URL" />
+      <button onClick={handleAnalysis}>Analyze Image</button>
+      <button onClick={handleGeneration}>Generate Image</button>
+    </div>
+  );
 }
 
 export default App;
