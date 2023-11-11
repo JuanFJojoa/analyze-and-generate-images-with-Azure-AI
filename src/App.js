ad8e596ea48eea8878b2e246d223060cd70e9f8a
Juan Jojoa <juanjojoag@gmail.com>
2023-11-11 17:35:34 +1100

    1

--- src/App.js
+++ src/App.js
@@ -1,43 +1,24 @@
 import React, { useState } from 'react';
-import { analyzeImage } from './azure-image-analysis';
 
 function App() {
-  const [url, setUrl] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [result, setResult] = useState(null);
+    const [url, setUrl] = useState('');
 
-  const handleAnalysis = async () => {
-    setLoading(true);
-    const analysisResult = await analyzeImage(url);
-    setResult(analysisResult);
-    setLoading(false);
-  };
+    const handleAnalysis = () => {
+        // Image analysis logic goes here
+    };
 
-  const handleGeneration = () => {
-    // Image generation logic goes here
-  };
+    const handleGeneration = () => {
+        // Image generation logic goes here
+    };
 
-  const DisplayResults = () => {
-    if (!result) return null;
     return (
-      <div>
-        <h2>Results</h2>
-        <p>{JSON.stringify(result, null, 2)}</p>
-        <img src={url} alt="Analyzed" />
-      </div>
+        <div>
+            <h1>Image Analyzer and Generator</h1>
+            <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} placeholder="Enter image URL" />
+            <button onClick={handleAnalysis}>Analyze Image</button>
+            <button onClick={handleGeneration}>Generate Image</button>
+        </div>
     );
-  };
-
-  return (
-    <div>
-      <h1>Image Analyzer and Generator</h1>
-      <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} placeholder="Enter image URL" />
-      <button onClick={handleAnalysis} disabled={loading}>Analyze Image</button>
-      <button onClick={handleGeneration}>Generate Image</button>
-      {loading && <p>Processing...</p>}
-      <DisplayResults />
-    </div>
-  );
 }
 
 export default App;
